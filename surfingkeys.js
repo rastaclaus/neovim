c8847da20f9c7646691d96ba0d8e5dfe91ac6d5d
Artem Shestopalov <tema.rastaclaus@gmail.com>
2018-07-27 14:10:26 +0300

    fix mod inline query for surfingkeys

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,7 +1,7 @@
 Front.registerInlineQuery({
 	url: `https://translate.yandex.net/api/v1.5/tr.json/translate
 				?key=trnsl.1.1.20171231T100146Z.4b564db549f21328.24eefc55e8411dd6a39d453dcec3d14eddd2844b
-				&lang=ru,
+				&lang=ru
 				&text=`,
 	parseResult: function(res) {
 		try {
